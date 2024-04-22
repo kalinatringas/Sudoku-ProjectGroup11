c4b4be3babc23cf8169bba65c7f5592902e1cbba
John Riley <145623701+jrile018@users.noreply.github.com>
2024-04-22 18:44:50 -0400

    Update sudoku_generator.py

--- sudoku_generator.py
+++ sudoku_generator.py
@@ -371,3 +371,87 @@ debug_test_board = debug_generate_sudoku()
 debug_test_board
 
 """
+
+
+"""
+import math
+import random
+
+class SudokuGenerator:
+    def __init__(self, row_length=9):
+        self.row_length = row_length
+        self.board = [[0] * row_length for _ in range(row_length)]
+        self.box_length = int(math.sqrt(row_length))
+        self.fill_values()
+
+    def get_board(self):
+        return self.board
+
+    def print_board(self):
+        for row in self.board:
+            print(" ".join(str(num) for num in row))
+
+    def valid_in_row(self, row, num):
+        return num not in self.board[row]
+
+    def valid_in_col(self, col, num):
+        for r in range(self.row_length):
+            if self.board[r][col] == num:
+                return False
+        return True
+
+    def valid_in_box(self, row_start, col_start, num):
+        box_start_row = row_start - row_start % 3
+        box_start_col = col_start - col_start % 3
+        for r in range(3):
+            for c in range(3):
+                if self.board[box_start_row + r][box_start_col + c] == num:
+                    return False
+        return True
+
+    def is_valid(self, row, col, num):
+        return (self.valid_in_row(row, num) and
+                self.valid_in_col(col, num) and
+                self.valid_in_box(row - row % 3, col - col % 3, num))
+
+    def fill_box(self, row_start, col_start):
+        nums = list(range(1, 10))
+        random.shuffle(nums)
+        for i in range(3):
+            for j in range(3):
+                self.board[row_start + i][col_start + j] = nums.pop()
+
+    def fill_diagonal(self):
+        for i in range(0, self.row_length, self.box_length):
+            self.fill_box(i, i)
+
+    def fill_remaining(self, row, col):
+        if col >= self.row_length:
+            row += 1
+            col = 0
+        if row >= self.row_length:
+            return True
+
+        if self.board[row][col] == 0:
+            for num in range(1, self.row_length + 1):
+                if self.is_valid(row, col, num):
+                    self.board[row][col] = num
+                    if self.fill_remaining(row, col + 1):
+                        return True
+                    self.board[row][col] = 0
+            return False
+        else:
+            return self.fill_remaining(row, col + 1)
+
+    def fill_values(self):
+        self.fill_diagonal()
+        self.fill_remaining(0, 0)  # Start from the first cell after diagonal
+
+def debug_generate_sudoku():
+    sudoku = SudokuGenerator()
+    sudoku.print_board()
+    return sudoku.get_board()
+
+# Testing the Sudoku generator
+debug_test_board = debug_generate_sudoku()
+"""
